a2673161dc1b1d4410d18652b3946eee22e016e2
Anthony Headley <agheadley@gmail.com>
2024-01-13 16:30:08 +0000

    a2

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
-import adapter from '@sveltejs/adapter-auto';
+//import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
+/*
 const config = {
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
@@ -9,5 +10,17 @@ const config = {
 		adapter: adapter()
 	}
 };
+*/
 
-export default config;
+//export default config;
+
+import adapter from '@sveltejs/adapter-vercel';
+
+export default {
+	kit: {
+		adapter: adapter({
+			// see below for options that can be set here
+		})
+	}
+};
+
