6d7c9098be268cefa3dae6c41eea2dac6c6fb9db
Anthony Headley <agheadley@gmail.com>
2024-01-14 06:25:50 +0000

    a10

--- svelte.config.js
+++ svelte.config.js
@@ -1,26 +1,13 @@
-//import adapter from '@sveltejs/adapter-auto';
-
-/** @type {import('@sveltejs/kit').Config} */
-/*
-const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
-};
-*/
-
-//export default config;
-
 import adapter from '@sveltejs/adapter-vercel';
 
+
+/* dangerous */
+/* https://github.com/sveltejs/kit/discussions/7600 */
+/* https://stackoverflow.com/questions/73790956/cross-site-post-form-submissions-are-forbidden/73801539#73801539 */
 export default {
 	kit: {
-		adapter: adapter({
-			// see below for options that can be set here
-		})
+		adapter: adapter({}),
+		csrf: {checkOrigin: false}
 	}
 };
 
