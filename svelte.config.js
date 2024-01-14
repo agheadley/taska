480987aff69a2d242133aebbed9550df3794f743
Anthony Headley <agheadley@gmail.com>
2024-01-14 06:53:17 +0000

    a14

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,23 @@
 import adapter from '@sveltejs/adapter-vercel';
 
 
-/* dangerous */
+export default {
+	kit: {
+		adapter: adapter({})
+	}
+};
+
+
+
+/* vercel dev only - dangerous */
 /* https://github.com/sveltejs/kit/discussions/7600 */
 /* https://stackoverflow.com/questions/73790956/cross-site-post-form-submissions-are-forbidden/73801539#73801539 */
+
+/*
 export default {
 	kit: {
 		adapter: adapter({}),
 		csrf: {checkOrigin: false}
 	}
 };
-
+*/
